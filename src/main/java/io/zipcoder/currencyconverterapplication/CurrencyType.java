782f2fb357778ba0fe8e4091f10741ffef58985f
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-07 11:35:21 -0500

    all 300 tests pass

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -1,23 +1,25 @@
 package io.zipcoder.currencyconverterapplication;
 
 public enum CurrencyType {
-    AUSTRALIAN_DOLLAR(2.70),
-    CANADIAN_DOLLAR(2.64),
-    CHINESE_YR(13.84),
-    EURO(1.88),
-    FRANC(2.02),
-    POUND(1.64),
-    RINGGIT(8.94),
-    RUPEE(136.64),
-    SINGAPORE_DOLLAR(1.86),
-    US_DOLLAR(2.0),
-    UNIVERSAL_CURRENCY(1.0),
-    YEN(231.68);
+    AUSTRALIAN_DOLLAR(2.70,"AustralianDollar"),
+    CANADIAN_DOLLAR(2.64, "CanadianDollar"),
+    CHINESE_YR(13.84, "ChineseYR"),
+    EURO(1.88, "Euro"),
+    FRANC(2.02, "Franc"),
+    POUND(1.64, "Pound"),
+    RINGGIT(8.94, "Ringgit"),
+    RUPEE(136.64, "Rupee"),
+    SINGAPORE_DOLLAR(1.86, "SingaporeDollar"),
+    US_DOLLAR(2.0, "USDollar"),
+    UNIVERSAL_CURRENCY(1.0, "UniversalCurrency"),
+    YEN(231.68, "Yen");
 
     private final double rate;
+    private final String type;
 
-    CurrencyType(double rate) {
+    CurrencyType(double rate, String type) {
         this.rate = rate;
+        this.type = type;
     }
 
     public Double getRate() {
@@ -25,6 +27,11 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+        for (CurrencyType ct : CurrencyType.values()) {
+            if (ct.type.equals(currency.getClass().getSimpleName())) {
+                return ct;
+            }
+        }
         return null;
     }
 }
